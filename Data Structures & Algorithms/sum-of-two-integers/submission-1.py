decd989e93b3a1460c6ef9c391b31c0e338d49a9
GABABAB <mahmoudsaad@hotmail.co.uk>
2026-06-27 20:18:42 +0100

    deleted duplicates

--- Data Structures & Algorithms/sum-of-two-integers/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def getSum(self, a: int, b: int) -> int:
-        return a+ b
-        carry = (a&b) << 1
-        while carry != 0:
-            carry = (a&b) << 1
-            number = a^b    
-            answer = number^carry
-        return answer
